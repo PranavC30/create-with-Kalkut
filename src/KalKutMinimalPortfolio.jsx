dbd15fbe9be9c4c3c37ceb1e8970d17847c7c77c
PranavC30 <chakravortypranav1@gmail.com>
2026-02-18 12:03:34 +0530

    ho ga

--- src/KalKutMinimalPortfolio.jsx
+++ src/KalKutMinimalPortfolio.jsx
@@ -222,22 +222,6 @@ const KalKutMinimalPortfolio = () => {
         </div>
       )}
 
-      {/* Animated Background Particles */}
-      <div className="particles">
-        {[...Array(20)].map((_, i) => (
-          <div
-            key={i}
-            className="particle"
-            style={{
-              left: `${Math.random() * 100}%`,
-              top: `${Math.random() * 100}%`,
-              animationDelay: `${Math.random() * 5}s`,
-              animationDuration: `${5 + Math.random() * 10}s`
-            }}
-          />
-        ))}
-      </div>
-
       {/* Custom cursor glow */}
       <div 
         className="cursor-glow"
